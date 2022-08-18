d71fc9d0b5b643d92c70553c13b775eea67db6f9
allisterb <allister.beharry@gmail.com>
2022-08-18 19:20:21 -0400

    Add Ceramic header.

--- src/reporting/reporting.cpp
+++ src/reporting/reporting.cpp
@@ -3,7 +3,7 @@
 using json = nlohmann::json;
 
 
-void report(const string* base_file,  std::vector<string> devices,  std::map<string, double> measurements)
+void report(const string* base_file,  std::vector<string> devices,  std::map<string, double> measurements, const string* ceramic_url, const string* did)
 {
     info("Loading base reporting data file {}...", *base_file);
     std::ifstream f(*base_file);
@@ -26,8 +26,34 @@ void report(const string* base_file,  std::vector<string> devices,  std::map<str
     data["date"] = dt;
     data["devices"] = devices;
     data["measurements"] = measurements;
-    std::string s = data.dump(); 
-    debug("JSON data document:{}", s);
+    std::string header = R"("header": { "family": "test"})";
+    std::string body = R"(
+    {
+      "type": 0,
+      "genesis": {
+        "header": {
+          "family": "test"
+        },
+        "pwr": 
+            )" + data.dump() + "} }";
+    
+    data = json::parse(body); 
+    string k = "did:key:" + (*did);
+    string controllers[] = {k};
+    data["genesis"]["header"]["controllers"] = controllers;
+    debug("JSON data document:{}", data.dump());
+
+    // HTTPS
+    httplib::Client client(*ceramic_url);
+    if (auto res = client.Post("/api/v0/streams", data.dump(), "application/json")) {
+      if (res->status == 200) {
+        std::cout << res->body << std::endl;
+      }
+    } else {
+      auto err = res.error();
+      std::cout << "HTTP error: " << httplib::to_string(err) << std::endl;
+  }
+    //r.
     //json data = R"(
   //{
   //  "location": {
@@ -37,6 +63,6 @@ void report(const string* base_file,  std::vector<string> devices,  std::map<str
   //  "devices":[]
   //}
 //)"_json;
-f.close();
+//f.close();
 
 }
--- src/reporting/reporting.h
+++ src/reporting/reporting.h
@@ -1,16 +1,15 @@
 #ifndef __INCLUDE_GUARD_REPORTING_H
 #define __INCLUDE_GUARD_REPORTING_H
 
-#include "../lib.h"
-#include "../json.hpp"
 #include <fstream>
-#include<iostream>
-#include<sstream>
+#include <iostream>
+#include <sstream>
 #include <ctime>
 
+#include "../lib.h"
+#include "../json.hpp"
 #include "../httplib.h"
-#include "../devices/device.h"
 
-void report(const string* base_file, std::vector<string> devices, std::map<string, double> measurements);
+void report(const string* base_file, std::vector<string> devices, std::map<string, double> measurements, const string* ceramic_url, const string* did);
 
 #endif
